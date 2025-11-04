4d755445c20b980b3ee5128031c3bd887e314d2b
周鸣轩 <657427356@qq.com>
2025-11-04 15:16:15 +0800

    组件定义更新

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -7,17 +7,22 @@ export { default as DateInput } from './input/DatePicker';
 export { default as DateRangeInput } from './input/DateRangePicker';
 
 // Aggregated Input namespace (Ant Design-like)
-import * as InputNs from './input/index';
 import DatePicker from './input/DatePicker';
 import DateRangePicker from './input/DateRangePicker';
 import TextArea from './input/TextArea';
 
+import Text from './input/Text';
+import Number from './input/Number';
+import Password from './input/Password';
+import Select from './input/Select';
+import TransferSelect from './input/TransferSelect';
+
 export const Input = {
-  Text: InputNs.Text,
-  Number: InputNs.Number,
-  Password: InputNs.Password,
-  Select: InputNs.Select,
-  TransferSelect: InputNs.TransferSelect,
+  Text: Text,
+  Number: Number,
+  Password: Password,
+  Select: Select,
+  TransferSelect: TransferSelect,
   Date: DatePicker,
   DateRange: DateRangePicker,
   TextArea: TextArea,
--- src/components/input/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as Text } from './Text';
-export { default as Number } from './Number';
-export { default as Password } from './Password';
-export { default as Select } from './Select';
-export { default as TransferSelect } from './TransferSelect';
-export { default as DateInput, DateRange } from './Date';
